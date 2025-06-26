83f00358fedb80bdfb5b1a4b2f97d2f755061de4
Hadiid Andri Yulison <hadiidandri2000@gmail.com>
2025-06-26 10:47:15 +0700

    first commit

---
+++ e-raport/data.php
@@ -0,0 +1,41 @@
+<?php
+// Data Siswa dan Nilai
+$nama_sekolah = "SMK Koding Nusantara";
+$kelas = "11-RPL";
+$wali_kelas = "Bapak Setiawan";
+
+$data_siswa = [
+    [
+        "nama" => "Andi Pratama",
+        "nilai" => [
+            "Matematika" => 85,
+            "Bahasa Indonesia" => 90,
+            "Bahasa Inggris" => 88
+        ]
+    ],
+    [
+        "nama" => "Bunga Citra",
+        "nilai" => [
+            "Matematika" => 65,
+            "Bahasa Indonesia" => 75,
+            "Bahasa Inggris" => 70
+        ]
+    ],
+    [
+        "nama" => "Charlie Darmawan",
+        "nilai" => [
+            "Matematika" => 92,
+            "Bahasa Indonesia" => 88,
+            "Bahasa Inggris" => 95
+        ]
+    ],
+    [
+        "nama" => "Diana Sari",
+        "nilai" => [
+            "Matematika" => 50,
+            "Bahasa Indonesia" => 65,
+            "Bahasa Inggris" => 60
+        ]
+    ]
+];
+?>
